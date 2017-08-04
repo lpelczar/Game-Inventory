4440b090d9cd5322f935dae37d7b6978551d9a26
Devmack <starzykdominik93@gmail.com>
2017-08-04 14:37:06 +0200

    add main

--- gameInventory.py
+++ gameInventory.py
@@ -1,4 +1,4 @@
-# This is the file where you must work. Write code in the functions, create new functions, 
+# This is the file where you must work. Write code in the functions, create new functions,
 # so they work according to the specification
 
 # Displays the inventory.
@@ -11,11 +11,11 @@ def add_to_inventory(inventory, added_items):
     pass
 
 
-# Takes your inventory and displays it in a well-organized table with 
+# Takes your inventory and displays it in a well-organized table with
 # each column right-justified. The input argument is an order parameter (string)
 # which works as the following:
 # - None (by default) means the table is unordered
-# - "count,desc" means the table is ordered by count (of items in the inventory) 
+# - "count,desc" means the table is ordered by count (of items in the inventory)
 #   in descending order
 # - "count,asc" means the table is ordered by count in ascending order
 def print_table(inventory, order=None):
@@ -23,7 +23,7 @@ def print_table(inventory, order=None):
 
 
 # Imports new inventory items from a file
-# The filename comes as an argument, but by default it's 
+# The filename comes as an argument, but by default it's
 # "import_inventory.csv". The import automatically merges items by name.
 # The file format is plain text with comma separated values (CSV).
 def import_inventory(inventory, filename="import_inventory.csv"):
@@ -32,7 +32,12 @@ def import_inventory(inventory, filename="import_inventory.csv"):
 
 # Exports the inventory into a .csv file.
 # if the filename argument is None it creates and overwrites a file
-# called "export_inventory.csv". The file format is the same plain text 
+# called "export_inventory.csv". The file format is the same plain text
 # with comma separated values (CSV).
 def export_inventory(inventory, filename="export_inventory.csv"):
+    pass
+
+
+# Main function sets initial variables and stores rest of the functions
+def main():
     pass
